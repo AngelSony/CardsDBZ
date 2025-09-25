82b0fbc82ad0df675f712f188b831e64709e79ca
AngelSony <angelfol@outlook.com>
2025-09-25 04:02:20 -0300

    Added class Message, for handling client-server messages.

---
+++ CardsDBZ/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsDBZ
+{
+    class Message
+    {
+        private object _Obj;
+        private Type _ObjType;
+
+        public Message() { }
+        public Message(object obj, Type type)
+        {
+            _Obj = obj;
+            _ObjType = type;
+        }
+        public object Obj { get => _Obj; set => _Obj = value; }
+        public Type ObjType { get => _ObjType; set => _ObjType = value; }
+    }
+}
