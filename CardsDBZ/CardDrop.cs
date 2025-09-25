3f869a9b89c22010fe0a426ad420a5b5bec12dcf
AngelSony <angelfol@outlook.com>
2025-09-25 04:36:15 -0300

    Added CardDrop class for handling card movement on the board

---
+++ CardsDBZ/CardDrop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsDBZ
+{
+    class CardDrop
+    {
+        private Card _Card;
+        private string _PictureBox;
+
+        public CardDrop() { }
+        public CardDrop(Card card, string pBox) : this()
+        {
+            _Card = card;
+            _PictureBox = pBox;
+        }
+        public string PictureBox { get => _PictureBox; set => _PictureBox = value; }
+        public Card Card { get => _Card; set => _Card = value; }
+    }
+}
