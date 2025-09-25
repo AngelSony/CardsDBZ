22863be9fcc8c834c3de1722f78e503ba02b28c1
AngelSony <angelfol@outlook.com>
2025-09-25 04:00:49 -0300

    Added Functions for json conversion

---
+++ CardsDBZ/Functions.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsDBZ
+{
+    static class Functions
+    {
+        public static string ObjectToString(object obj)
+        {
+            string json = JsonConvert.SerializeObject(obj);
+
+            byte[] bytes = Encoding.Default.GetBytes(json);
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static T StringToObject<T>(string base64String)
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+
+            string json = Encoding.Default.GetString(bytes);
+
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
