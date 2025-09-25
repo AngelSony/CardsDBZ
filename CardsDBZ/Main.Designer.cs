3aaf7571c5fe3757854ee6754a8527c2c5967d47
AngelSony <angelfol@outlook.com>
2025-09-25 04:09:45 -0300

    Added Main form

---
+++ CardsDBZ/Main.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace CardsDBZ
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblIp = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.btnHost = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.tlpMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 4;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 14F));
+            this.tlpMain.Controls.Add(this.btnJoin, 1, 2);
+            this.tlpMain.Controls.Add(this.btnPlay, 1, 5);
+            this.tlpMain.Controls.Add(this.btnExit, 1, 6);
+            this.tlpMain.Controls.Add(this.lblIp, 1, 3);
+            this.tlpMain.Controls.Add(this.lblPort, 1, 4);
+            this.tlpMain.Controls.Add(this.txtIp, 2, 3);
+            this.tlpMain.Controls.Add(this.btnHost, 1, 1);
+            this.tlpMain.Controls.Add(this.txtPort, 2, 4);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 8;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tlpMain.Size = new System.Drawing.Size(874, 579);
+            this.tlpMain.TabIndex = 0;
+            // 
+            // btnJoin
+            // 
+            this.tlpMain.SetColumnSpan(this.btnJoin, 2);
+            this.btnJoin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnJoin.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJoin.Location = new System.Drawing.Point(10, 125);
+            this.btnJoin.Margin = new System.Windows.Forms.Padding(0);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Padding = new System.Windows.Forms.Padding(1);
+            this.btnJoin.Size = new System.Drawing.Size(850, 115);
+            this.btnJoin.TabIndex = 1;
+            this.btnJoin.Text = "Unirse";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // btnPlay
+            // 
+            this.tlpMain.SetColumnSpan(this.btnPlay, 2);
+            this.btnPlay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnPlay.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(10, 336);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(0);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Padding = new System.Windows.Forms.Padding(1);
+            this.btnPlay.Size = new System.Drawing.Size(850, 115);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Jugar";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.tlpMain.SetColumnSpan(this.btnExit, 2);
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnExit.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(10, 451);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(1);
+            this.btnExit.Size = new System.Drawing.Size(850, 115);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Salir";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblIp
+            // 
+            this.lblIp.AutoSize = true;
+            this.lblIp.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblIp.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIp.Location = new System.Drawing.Point(128, 240);
+            this.lblIp.Name = "lblIp";
+            this.lblIp.Size = new System.Drawing.Size(49, 48);
+            this.lblIp.TabIndex = 4;
+            this.lblIp.Text = "IP:";
+            this.lblIp.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblPort.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPort.Location = new System.Drawing.Point(40, 288);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(137, 48);
+            this.lblPort.TabIndex = 5;
+            this.lblPort.Text = "PUERTO:";
+            this.lblPort.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtIp
+            // 
+            this.txtIp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIp.Location = new System.Drawing.Point(182, 242);
+            this.txtIp.Margin = new System.Windows.Forms.Padding(2);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(676, 44);
+            this.txtIp.TabIndex = 6;
+            // 
+            // btnHost
+            // 
+            this.tlpMain.SetColumnSpan(this.btnHost, 2);
+            this.btnHost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnHost.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHost.Location = new System.Drawing.Point(10, 10);
+            this.btnHost.Margin = new System.Windows.Forms.Padding(0);
+            this.btnHost.Name = "btnHost";
+            this.btnHost.Padding = new System.Windows.Forms.Padding(1);
+            this.btnHost.Size = new System.Drawing.Size(850, 115);
+            this.btnHost.TabIndex = 0;
+            this.btnHost.Text = "Crear Partida";
+            this.btnHost.UseVisualStyleBackColor = true;
+            this.btnHost.Click += new System.EventHandler(this.btnHost_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(182, 290);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(676, 44);
+            this.txtPort.TabIndex = 7;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 579);
+            this.Controls.Add(this.tlpMain);
+            this.Name = "formMain";
+            this.Text = "Menú Principal";
+            this.tlpMain.ResumeLayout(false);
+            this.tlpMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.Button btnHost;
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblIp;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.TextBox txtPort;
+    }
+}
+
---
+++ CardsDBZ/Main.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardsDBZ
+{
+    public partial class formMain : Form
+    {
+        private string playerType;
+        private Connection conn;
+        public formMain()
+        {
+            InitializeComponent();
+
+            txtIp.Text = Connection.MyLocalIp();
+            txtPort.Text = "25255";
+        }
+        private async Task Host(int port)
+        {
+
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            playerType = "Client";
+            int port = int.Parse(txtPort.Text);
+            string ip = txtIp.Text;
+
+            //Open formGame
+            //this.Hide();
+        }
+        private void btnHost_Click(object sender, EventArgs e)
+        {
+            playerType = "Host";
+            int port = int.Parse(txtPort.Text);
+            string ip = txtIp.Text;
+            //Host game
+
+            //Open formGame
+
+            //this.Hide();
+        }
+    }
+}
