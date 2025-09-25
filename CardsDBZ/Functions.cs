2e8c1e7b421a483ed1629f83aa3f434a0efcec8b
AngelSony <angelfol@outlook.com>
2025-09-25 04:39:38 -0300

    Added shuffle function

--- CardsDBZ/Functions.cs
+++ CardsDBZ/Functions.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,22 @@ namespace CardsDBZ
 {
     static class Functions
     {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int n = list.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
         public static string ObjectToString(object obj)
         {
             string json = JsonConvert.SerializeObject(obj);
@@ -17,7 +34,6 @@ namespace CardsDBZ
 
             return Convert.ToBase64String(bytes);
         }
-
         public static T StringToObject<T>(string base64String)
         {
             byte[] bytes = Convert.FromBase64String(base64String);
